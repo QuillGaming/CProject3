10b8d0f2d7998115cb4019fec30c94b6cdebe346
Asher Antrim <aantrim246@cedarville.edu>
2025-04-27 20:21:58 -0400

    Think I got the if stmt right

--- src/parser/ASTNodes/IfStmt.java
+++ src/parser/ASTNodes/IfStmt.java
@@ -1,6 +1,8 @@
 package parser.ASTNodes;
 
-import lowlevel.Function;
+import lowlevel.*;
+import lowlevel.Operand.OperandType;
+import lowlevel.Operation.OperationType;
 
 public class IfStmt extends Statement {
     Statement thenStmt;
@@ -16,7 +18,6 @@ public class IfStmt extends Statement {
         System.out.println("    If Statement");
         System.out.println("     Condition (Expression)");
 
-
         System.out.println("     Then Branch (Statement)");
         thenStmt.printAST();
 
@@ -27,7 +28,65 @@ public class IfStmt extends Statement {
     }
 
     @Override
-    public void genLLCode(Function currItem) {
+    public void genLLCode(Function currFunc) {
+        // 1 make 2/3 blocks
+        BasicBlock thenBlock = new BasicBlock(currFunc);
+        BasicBlock postBlock = new BasicBlock(currFunc);
+        BasicBlock elseBlock = null;
+        if (elseStmt != null) {
+            elseBlock = new BasicBlock(currFunc);
+        }
 
+        // 2 gencode expr
+        Object exprResult = expr.genLLCode(currFunc.getCurrBlock());
+        int conditionRegNum;
+
+        // Get register number
+        if (exprResult instanceof Operand) {
+            conditionRegNum = ((Integer) ((Operand) exprResult).getValue()).intValue();
+        } else {
+            Operation op = (Operation) exprResult;
+            conditionRegNum = ((Integer) op.getDestOperand(0).getValue()).intValue();
+        }
+
+        // 3 create branch
+        BasicBlock targetBlock = (elseStmt != null) ? elseBlock : postBlock;
+        Operation branchOp = new Operation(OperationType.BEQ, currFunc.getCurrBlock());
+        branchOp.setSrcOperand(0, new Operand(OperandType.REGISTER, Integer.valueOf(conditionRegNum)));
+        branchOp.setSrcOperand(1, new Operand(OperandType.INTEGER, Integer.valueOf(0)));
+        branchOp.setSrcOperand(2, new Operand(OperandType.BLOCK, Integer.valueOf(targetBlock.getBlockNum())));
+        currFunc.getCurrBlock().appendOper(branchOp);
+
+        // 4 append then
+        currFunc.getLastBlock().setNextBlock(thenBlock);
+        thenBlock.setPrevBlock(currFunc.getLastBlock());
+        currFunc.setLastBlock(thenBlock);
+
+        // 5 cb = then
+        currFunc.setCurrBlock(thenBlock);
+
+        // 6 codegen then
+        thenStmt.genLLCode(currFunc);
+
+        // 7 append post
+        Operation jumpToPost = new Operation(OperationType.JMP, currFunc.getCurrBlock());
+        jumpToPost.setSrcOperand(0, new Operand(OperandType.BLOCK, Integer.valueOf(postBlock.getBlockNum())));
+        currFunc.getCurrBlock().appendOper(jumpToPost);
+
+        // 8 - 11
+        if (elseStmt != null) {
+            currFunc.appendUnconnectedBlock(elseBlock);
+            currFunc.setCurrBlock(elseBlock);
+            elseStmt.genLLCode(currFunc);
+            Operation jumpFromElse = new Operation(OperationType.JMP, currFunc.getCurrBlock());
+            jumpFromElse.setSrcOperand(0, new Operand(OperandType.BLOCK, Integer.valueOf(postBlock.getBlockNum())));
+            currFunc.getCurrBlock().appendOper(jumpFromElse);
+        }
+
+        // 12 cb = post
+        currFunc.getLastBlock().setNextBlock(postBlock);
+        postBlock.setPrevBlock(currFunc.getLastBlock());
+        currFunc.setLastBlock(postBlock);
+        currFunc.setCurrBlock(postBlock);
     }
 }
